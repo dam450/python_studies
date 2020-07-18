73caec1d064d14c411e682e9038b46c982affe00
dam450 <68165752+dam450@users.noreply.github.com>
2020-07-18 20:17:34 -0300

    criacao de classes e metodos
    Signed-off-by: dam450 <68165752+dam450@users.noreply.github.com>

---
+++ aula7_calculadora1.py
@@ -0,0 +1,24 @@
+#definindo uma classe em python
+class Calculadora: #por convencao classes comecam com letra maiuscula
+    def __init__(self, num1, num2):
+        self.valor_a = num1
+        self.valor_b = num2
+
+    #definindo metodos em python
+    def soma(self):  #metodos com letra minuscula
+        return self.valor_a + self.valor_b
+
+    def subtracao (self):
+        return self.valor_a - self.valor_b
+
+    def multiplicacao (self):
+        return self.valor_a * self.valor_b
+
+    def divisao (self):
+        return self.valor_a / self.valor_b
+
+
+calculadora = Calculadora(10, 2)
+print(calculadora.valor_b)
+print(calculadora.soma())
+print(calculadora.divisao())
