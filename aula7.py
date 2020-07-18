a40a132747be0ad5c931c53cc32a40964205eadd
dam450 <68165752+dam450@users.noreply.github.com>
2020-07-18 19:20:06 -0300

    definicao de metodo em python (aula 7)
    Signed-off-by: dam450 <68165752+dam450@users.noreply.github.com>

---
+++ aula7.py
@@ -0,0 +1,17 @@
+
+#definindo metodos em python
+def soma(a , b):
+    return a + b
+
+def subtracao (a ,b):
+    return a - b
+
+def multiplicacao (a ,b):
+    return a * b
+
+def divisao (a ,b):
+    return a / b
+
+
+print(soma(1,2))
+print(subtracao(4,7))
